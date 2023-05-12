59af76d956a4dd4c1dfb8d99f0186e09c39cd359
ErckLima <erickfalubay@gmail.com>
2023-05-12 17:13:28 -0300

    3.0
    Incluindo a opção de terceiros para fazer o lançamento de contas que tem outra pessoa para pagar também

--- js.js
+++ js.js
@@ -3,6 +3,7 @@ const dados_todos = [];
 function safe(){
 
   this.formatar();
+  this.formatar2();
   if(this.validarcampos() === 'vazio'){
     
   }
@@ -12,19 +13,19 @@ function safe(){
   const Descrição = document.getElementById("Descrição").value
   // const forma = document.getElementById("forma").value  --------------------------Descontinuado
   const valor = document.getElementById("numnegativo").value.toString().replace(".",",");
-  
+  const Terceiro = document.getElementById("valoroculto").value.toString().replace(".",",");
   const Parcela_mes = document.getElementById("Parcela_mes").value
   const Parcela_total = document.getElementById("Parcela_total").value
   const Data = document.getElementById("Data").value
 
 
-  const dados = {Descrição, valor, Parcela_mes, Parcela_total, Data}
+  const dados = {Descrição, valor, Terceiro, Parcela_mes, Parcela_total, Data}
 
   dados_todos.push(dados)
 
   let mostrar_tudo = '';
   dados_todos.map((todos) => {
-     mostrar_tudo += '<h1> --------<br>' + todos.Descrição + ' <br> ' + todos.valor + ' <br>[' + todos.Parcela_mes + ' / ' + todos.Parcela_total + ']' + '<br>' + todos.Data +  '</h1>';
+     mostrar_tudo += '<h1> --------<br>' + todos.Descrição + '<br>' + todos.Terceiro +  ' <br> ' + todos.valor + ' <br>[' + todos.Parcela_mes + ' / ' + todos.Parcela_total + ']' + '<br>' + todos.Data +  '</h1>';
     const print = document.getElementById("mostrar");
     print.innerHTML = mostrar_tudo;
   })
@@ -151,6 +152,16 @@ function Carregadata(){
   var dateStr = yyyy + '-' + mm + '-' + dd;
   document.getElementById("Data").value = dateStr;
 
+  const che = document.getElementsByName("Terceiros");
+  let alterarselecionado = document.getElementById("val");
+
+  if(che[1].checked){
+    alterarselecionado.style.display ="none";
+    
+
+
+  }
+
 }
 
 function recarregarPagina() {
@@ -223,5 +234,63 @@ function formatar(){
     campoInput.value = campo; // atualiza o valor do campo
 };
 
+
+/////////////////////////////////////////////////////////////////////////////////////
+//FUNÇÃO PARA FORMATAR O INPUT DO VALOR COM OS PONTOS
+
+
+const novocampotalvez = document.getElementById('valoroculto');
+
+function formatar2(){
+    let novocampo = novocampotalvez.value;
+
+    // Remove espaços em branco e substitui ponto por vírgula para permitir a formatação correta
+    novocampo = novocampo.trim().replace('.', ',');
+
+    // Verifica o comprimento do número
+    if (novocampo.length <= 2) {
+      // Insere zero e vírgula no início
+      novocampo = '0,' + novocampo.padStart(2, '0');
+    } else {
+      // Remove zeros à esquerda
+      novocampo = novocampo.replace(/^0+/, '');
+      
+      // Verifica se já existe uma vírgula no número
+      const posicaoVirgula = novocampo.indexOf(',');
+      if (posicaoVirgula === -1) {
+        // Se não existir, insere vírgula após as duas primeiras casas decimais
+        novocampo = novocampo.substring(0, novocampo.length - 2) + ',' + novocampo.substring(novocampo.length - 2);
+      } else if (posicaoVirgula === novocampo.length - 3) {
+        // Se já existir e estiver na posição correta, não faz nada
+      } else {
+        // Se já existir e estiver em outra posição, remove a vírgula existente e insere na posição correta
+        novocampo = novocampo.replace(',', '');
+        novocampo = novocampo.substring(0, novocampo.length - 2) + ',' + novocampo.substring(novocampo.length - 2);
+      }
+    }
+
+    // Substitui vírgula por ponto para exibir a formatação correta
+    novocampo = novocampo.replace(',', '.');
+
+    novocampotalvez.value = novocampo; // atualiza o valor do campo
+};
+//////////////////////////////////////////////////////////////////////////////////
+
+
+function oculta() {
+  const selecionado = document.getElementsByName("Terceiros");
+  let alterarselecionado = document.getElementById("val");
+
+  if(selecionado[0].checked){
+    alterarselecionado.style.display ="block";
+
+
+  }
+  else if(selecionado[1].checked){
+    let valoroculto = document.getElementById("valoroculto");
+    valoroculto.innerHTML.value = 0;
+    alterarselecionado.style.display ="none";
+  }
+}
 
 // TESTES TESTES TESTES TESTES TESTES TESTES TESTES TESTES TESTES TESTES TESTES TESTES TESTES
